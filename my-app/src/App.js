e112b1bf9205b249c23babf62656aa18f5415a80
mehedimahid <53916831+mehedimahid@users.noreply.github.com>
2023-06-04 09:56:25 +0600

    Forwarding Refs

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,11 @@
 import "./App.css";
-// import logo from './logo.svg'
-import Hello from "./component/Hello";
-// import { Hello } from './component/Hello';
-import Welcome from "./component/Welcome";
-import Hellojsx from "./component/whyJSX";
-import Withoutjsx from "./component/whyJSX";
-import Message from "./component/Message";
-import Counter from "./component/Counter";
-import FunctionClick from "./component/FunctionClick";
-import ClassClick from "./component/ClassClick";
-import EventBind from "./component/EventBind";
-import ParentComponent from "./component/ParentComponent";
-import UserGreating from "./component/UserGreating";
-import NameList from "./component/NameList";
-import StyleSheet from "./component/styleWithCSS/StyleSheet";
-import Inline from "./component/styleWithCSS/Inline";
-import "./component/styleWithCSS/appStyle.css";
-import styles from "./component/styleWithCSS/appStyle.module.css";
-import Form from "./component/Form";
-import LifeCycleA from "./component/LifeCycle/LifeCycleA";
-import ComponentUpdate from "./component/LifeCycle/ComponentUpdate";
-import FragmentDemo from "./component/Fragment/FragmentDemo";
-import Table from "./component/Fragment/Table";
-import RefsDemo from "./component/Refs/RefsDemo";
-import FocusInput from "./component/Refs/FocusInput";
+
+import FRParentInput from "./component/Refs/FRParentInput";
 
 function App() {
   return (
     <div className="App">
-      <FocusInput />
+      <FRParentInput />
     </div>
   );
 }
