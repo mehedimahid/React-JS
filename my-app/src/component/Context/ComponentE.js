82ac5eb97443b4f83d3738d6bdf2a6a277452e8d
mehedimahid <53916831+mehedimahid@users.noreply.github.com>
2023-07-08 10:40:45 +0600

    Context

--- my-app/src/component/Context/ComponentE.js
+++ my-app/src/component/Context/ComponentE.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 import ComponentF from "./ComponentF";
+import userContext from "./UserContext";
 
 export class ComponentE extends Component {
+  static contextType = userContext;
   render() {
-    return <ComponentF />;
+    return (
+      <div>
+        Component E contextType {this.context}
+        <ComponentF />;
+      </div>
+    );
   }
 }
-
+// ComponentE.contextType = userContext;
 export default ComponentE;
--- my-app/src/component/Context/UserContext.js
+++ my-app/src/component/Context/UserContext.js
@@ -1,8 +1,9 @@
 import React from "react";
 
-const userContext = React.createContext();
+const userContext = React.createContext("Guest"); //parameter a defoult value set kora jay
 
 const UserProvider = userContext.Provider;
 const UserConsumer = userContext.Consumer;
 
 export { UserConsumer, UserProvider };
+export default userContext;
